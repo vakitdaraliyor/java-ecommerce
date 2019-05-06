cfe1b5e6e2f39b3cb4a40d28b1629669a3e7a860
Osman Savas <osmansavas93@hotmail.com>
2019-05-06 16:08:20 +0300

    Updated

--- ecommercefront/src/main/java/com/osman/ecommercefront/controller/PageController.java
+++ ecommercefront/src/main/java/com/osman/ecommercefront/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.osman.ecommercefront.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,6 +13,23 @@ public class PageController {
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to Spring Web Mvc");
+		return mv;
+	}
+	
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam("greeting") String greeting){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test2(@PathVariable("greeting") String greeting){
+		if(greeting == null){
+			greeting = "Hello PathVariable";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
 		return mv;
 	}
 
